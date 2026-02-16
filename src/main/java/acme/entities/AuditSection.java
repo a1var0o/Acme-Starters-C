2a8591632ddfa0b76e73448d6514e940840c0355
MarcosValleArias <marvalari@alum.us.es>
2026-02-16 10:59:27 +0100

    Implements models fro student 5

---
+++ src/main/java/acme/entities/AuditSection.java
@@ -0,0 +1,48 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.datatypes.SectionKind;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditSection extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	// @ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	// @ValidText
+	@Column
+	private String				notes;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Column
+	private Integer				hours;
+
+	@Mandatory
+	@Valid
+	@Column
+	private SectionKind			kind;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private AuditReport			auditReport;
+
+}
---
+++ src/main/java/acme/realms/Auditor.java
@@ -0,0 +1,46 @@
+/*
+ * Consumer.java
+ *
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.validation.Mandatory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Auditor extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	//@ValidHeader
+	@Column
+	private String				firm;
+
+	@Mandatory
+	//@ValidHeader
+	@Column
+	private String				highlights;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				solicitor;
+
+}
