4be5d20d120b7ffcaedde41924ea47e83eac4636
alvaro <alvvilale@alum.us.es>
2026-02-23 12:03:24 +0100

    fix: Minor fixes and clean up

--- src/main/java/acme/entities/InventionRepository.java
+++ src/main/java/acme/entities/InventionRepository.java
@@ -9,7 +9,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface InventionRepository extends AbstractRepository {
 
-	@Query("select sum(p.cost.amount) from Part p  where p.invention.id = :id")
-	Double computeTotalCost(int id);
+	@Query("select sum(p.cost.amount) from Part p  where p.invention.id = :inventionId")
+	Double computeTotalCost(int inventionId);
 
 }
