e6ca7ad13f829f708b077e66073bced590dd923f
León Martín <alemarlam@alum.us.es>
2026-03-15 19:23:52 +0100

    backend code for realm specific show

---
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipController.java
@@ -0,0 +1,26 @@
+
+package acme.features.sponsor.sponsorship;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.Sponsorship;
+import acme.realms.Sponsor;
+
+@Controller
+public class SponsorSponsorshipController extends AbstractController<Sponsor, Sponsorship> {
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("list", SponsorSponsorshipListService.class);
+		super.addBasicCommand("show", SponsorSponsorshipShowService.class);
+	}
+
+}
